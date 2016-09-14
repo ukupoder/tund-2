b5712604e9521fcc22971f0529b875c85f7df446
ukupode <ukupode@greeny.cs.tlu.ee>
2016-09-14 14:48:54 +0300

    added if and for loop

---
+++ second.php
@@ -0,0 +1,27 @@
+<?php
+	//echo ("test");	
+
+	$age = 5;
+	
+	if ($age < 18)
+	{
+		echo "alaealine";	
+	}
+	else 
+	{
+		echo "täisealine";
+	}
+	
+?>
+
+<br>
+
+<?php
+
+	for ($i = 1; $i <= $age; $i++)
+	{
+		echo $i."palju ";
+		
+	}
+		echo "Õnne";
+?>
